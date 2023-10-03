1a1c37e54a1273589705cc2dfa98f030187249c8
Abadines <annabelabadines479@gmail.com>
2023-10-03 15:31:09 +0800

    Added students table migration

--- database/migrations/2023_10_03_072918_create_students_table.php
+++ database/migrations/2023_10_03_072918_create_students_table.php
@@ -9,27 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void 
-    { 
+    public function up(): void
+    {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('midle_name')->nullable();
-            $table->date('birth_date');
-            $table->string('course'); 
-            $table->integer('year');
-            $table->char('section', 1);
-            $table->string('email')->unique();
             $table->timestamps();
-        });      
+        });
     }
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
-    { 
+    {
         Schema::dropIfExists('students');
     }
 };
